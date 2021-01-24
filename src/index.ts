22f6e8ba4449faf3b0542b879f22029e1d058605
Geoff Whitehead <geoff1012@gmail.com>
2021-01-24 23:23:27 +0000

    tidy

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,16 @@ type GetYInterceptProps = {
   gradient: number;
 };
 
-const getYIntercept = ({ plot, gradient }: GetYInterceptProps): number => {
-  const isCurrentMonth = plot.x === 0;
+const getYIntercept = ({
+  plot: { x, y },
+  gradient,
+}: GetYInterceptProps): number => {
+  const isCurrentMonth = x === 0;
 
   if (isCurrentMonth) {
-    return plot.y;
+    return y;
   } else {
-    return gradient * plot.x - plot.y;
+    return gradient * x - y;
   }
 };
 
@@ -36,10 +39,12 @@ export const accountTypeChecker = (
     return AccountType.fixed;
   }
 
-  const plots: Plot[] = accountBalanceHistory.map((entry) => ({
-    x: entry.monthNumber,
-    y: entry.account.balance.amount,
-  }));
+  const plots: Plot[] = accountBalanceHistory.map(
+    ({ monthNumber, account }) => ({
+      x: monthNumber,
+      y: account.balance.amount,
+    })
+  );
 
   const changeY = plots[plots.length - 1].y - plots[0].y;
   const changeX = plots[plots.length - 1].x - plots[0].x;
@@ -47,9 +52,9 @@ export const accountTypeChecker = (
 
   const yIntercept = getYIntercept({ plot: plots[0], gradient });
 
-  const isVariable = plots.some((plot) => {
-    const linearY = gradient * plot.x + yIntercept;
-    return plot.y !== linearY;
+  const isVariable = plots.some(({ x, y }) => {
+    const linearY = gradient * x + yIntercept;
+    return y !== linearY;
   });
 
   return isVariable ? AccountType.variable : AccountType.fixed;
